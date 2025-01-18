f9d78d58191b75617ee9fbed8cae154d71ed3c5f
Hezzie <hezronchelimo.hc@gmail.com>
2025-01-18 10:59:46 +0300

    dependency updates

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,5 +1,5 @@
 import { applyMiddleware, compose, createStore } from 'redux'
-import thunkMiddleware from 'redux-thunk'
+import {thunk as thunkMiddleware} from 'redux-thunk'
 
 import monitorReducersEnhancer from './enhancers/monitorReducer'
 import loggerMiddleware from './middleware/logger'
--- src/store/constants/index.js
+++ src/store/constants/index.js
@@ -1,3 +1,4 @@
+
 export const OPEN_API_MODEL = "gpt-3.5-turbo-0301";
 
 export const OPEN_API_USER = "user";
@@ -5,15 +6,13 @@ export const OPEN_API_USER = "user";
 export const OPEN_API_AUTHORIZATION = `Bearer ${process.env.REACT_APP_OPEN_AI_AUTHORIZATION}`;
 
 export const GENERATE_IMAGE_TAG_SUCCESS = "GENERATE_IMAGE_TAG_SUCCESS";
-
 export const GENERATE_IMAGE_TAG_FAILURE = "GENERATE_IMAGE_TAG_FAILURE";
 
 export const OPEN_AI_CHAT_BASE_URL =
-  "https://api.openai.com/v1/chat/completions";
+  "https://api.openai.com/v1/chat/completions";  
 
 export const OPEN_AI_TEXT_BASE_URL = "https://api.openai.com";
-
-export const OPEN_AI_TEXT_TAG_BASE_URL = `${OPEN_AI_TEXT_BASE_URL}/v1/engines/davinci/completions`;
+export const OPEN_AI_TEXT_TAG_BASE_URL = `${OPEN_AI_TEXT_BASE_URL}/v1/engines/gpt-3.5-turbo/completions`;
 
 export const OPEN_AI_GENERATOR_BASE_URL = `${OPEN_AI_TEXT_BASE_URL}/v1/images/generations`;
 
